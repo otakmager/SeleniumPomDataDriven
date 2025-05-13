efa622383d2febf8af341d4428b5c2b2c9dabc6a
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-05-13 12:11:38 +0700

    Add PageElements interface

---
+++ src/main/java/pageObjects/PageElements.java
@@ -0,0 +1,23 @@
+package pageObjects;
+
+public interface PageElements {
+    String topBarId = "topbar";
+    String digitalDutchLogoId = "digitaldutch_logo";
+    String titleHeaderId = "title";
+    String converterTitleHeaderId = "quantity-title";
+
+    String emailFooterXPath = "//a[starts-with(@href, 'mailto:')]";
+    String whatsNewFooterXPath = "//a[text()='What\'s New']";
+    String tipsFooterXPath = "//a[text()='Tips']";
+    String disclaimerFooterXPath = "//a[text()='Disclaimer']";
+
+    String whatsNewDivXPath = "//div[@id='whatsNew']";
+    String tipsDivXPath = "//div[@id='tips']";
+    String disclaimerDivXPath = "//div[@id='disclaimer']";
+
+    String inputFromXPath = "//input[@id='inputFrom']";
+    String selectUnitFromXPath = "//select[@id='selectFrom']";
+
+    String inputToXPath = "//input[@id='inputTo']";
+    String selectUnitToXPath = "//select[@id='selectTo]";
+}
