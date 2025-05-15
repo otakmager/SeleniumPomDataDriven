7fdf22c272a35fecad2ba0f2da75b4b53efac2fe
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-05-15 11:43:16 +0700

    feat: time converter tests

--- src/test/java/testCases/time/TimeConverterTest.java
+++ src/test/java/testCases/time/TimeConverterTest.java
@@ -1,15 +1,38 @@
 package testCases.time;
 
 import base.BaseTest;
+import org.testng.annotations.Test;
+import pageEvents.WebPageEvents;
 import utils.Constants;
 
 public class TimeConverterTest extends BaseTest {
+    private final String title = "Convert Time";
+    WebPageEvents events = new WebPageEvents();
+
     @Override
     protected String getUrl() {
         return Constants.timeConverterUrl;
     }
-    // 1. hour to minute
-    // 2. minute to second
-    // 3. second to millisecond
+
+    @Test
+    public void testTitle() {
+        events.verifyPageTitle(title);
+    }
+
+    @Test
+    public void testVerifyConverterTitleHeader() {
+        events.verifyConverterTitleHeader(title);
+    }
+
+    @Test
+    public void testConverter() {
+        Double value1 = 1.57;
+        Double value2 = 94.2;
+        String fromUnit = "hours [h]";
+        String toUnit = "minutes [min]";
+
+        events.changeOptionDigits(Constants.maxDigits);
+        events.testInputConverter(value1, value2, fromUnit, toUnit);
+    }
 
 }
