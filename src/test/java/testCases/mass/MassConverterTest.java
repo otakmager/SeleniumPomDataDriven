c8ffc17699c69ea934f84b0e73f5ceed420b5014
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-05-15 11:35:42 +0700

    feat: mass converter tests

--- src/test/java/testCases/mass/MassConverterTest.java
+++ src/test/java/testCases/mass/MassConverterTest.java
@@ -1,16 +1,38 @@
 package testCases.mass;
 
 import base.BaseTest;
+import org.testng.annotations.Test;
+import pageEvents.WebPageEvents;
 import utils.Constants;
 
 public class MassConverterTest extends BaseTest {
+    private final String title = "Convert Mass";
+    WebPageEvents events = new WebPageEvents();
+
     @Override
     protected String getUrl() {
         return Constants.massConverterUrl;
     }
-    // 1. gram to kilogram
-    // 2. kilogram to gram
-    // 3. milligram to gram
-    // 4. gram to milligram
+
+    @Test
+    public void testTitle() {
+        events.verifyPageTitle(title);
+    }
+
+    @Test
+    public void testVerifyConverterTitleHeader() {
+        events.verifyConverterTitleHeader(title);
+    }
+
+    @Test
+    public void testConverter() {
+        Double value1 = 325.75;
+        Double value2 = 325750.0;
+        String fromUnit = "grams [g]";
+        String toUnit = "milligrams [mg]";
+
+        events.changeOptionDigits(Constants.maxDigits);
+        events.testInputConverter(value1, value2, fromUnit, toUnit);
+    }
 
 }
